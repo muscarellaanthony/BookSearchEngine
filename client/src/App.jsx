2f475c362d5c190d092c78271da33a60cb62fccd
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-06-10 19:27:14 -0400

    final touches

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({ 
+  url: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
